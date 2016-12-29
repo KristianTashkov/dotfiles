5eaf516e7db8556e684d98c7156fdc150a311ae1
KristianTashkov <kristian@hyperscience.com>
2016-12-29 16:17:27 +0200

    Install file downloading prerequisites

---
+++ install.sh
@@ -0,0 +1,10 @@
+# Git Aware Prompt
+mkdir ~/.bash
+cd ~/.bash
+git clone git://github.com/jimeh/git-aware-prompt.git
+
+# Git Completion
+curl http://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash --output ~/git-completion.bash
+
+# tmux package manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
