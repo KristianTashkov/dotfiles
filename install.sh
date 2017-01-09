aa523a20cd23447105aadb0efcf1fa7cd9aceeba
KristianTashkov <kristian@hyperscience.com>
2017-01-09 10:58:04 +0200

    Symlink dotfiles

--- install.sh
+++ install.sh
@@ -1,10 +1,20 @@
+DIR=`pwd`
 # Git Aware Prompt
-mkdir ~/.bash
-cd ~/.bash
-git clone git://github.com/jimeh/git-aware-prompt.git
+git clone git://github.com/jimeh/git-aware-prompt.git ~/.bash
 
 # Git Completion
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash --output ~/git-completion.bash
 
 # tmux package manager
+mkdir -p ~/.tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# symlinks
+ln -s `pwd`/.tmux.conf ~/.tmux.conf
+ln -s `pwd`/.gitconfig ~/.gitconfig
+ln -s $DIR/.vimrc ~/.vimrc
+if [ "$(uname)" == "Darwin" ]; then
+    ln -s `pwd`/.bashrc ~/.bashrc
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    ln -s `pwd`/.bashrc ~/.extrabashrc
+fi
