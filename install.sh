1870d6afa4331ff9f3c35d95edfde34005b4ba5d
Kristian Tashkov <kristian.tashkov@gmail.com>
2022-10-30 22:02:22 +0200

    update dotfiles

--- install.sh
+++ install.sh
@@ -1,10 +1,4 @@
 DIR=`pwd`
-# Git Aware Prompt
-git clone git://github.com/jimeh/git-aware-prompt.git ~/.bash
-
-# Git Completion
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash --output ~/git-completion.bash
-
 # tmux package manager
 mkdir -p ~/.tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -13,4 +7,3 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -s `pwd`/.tmux.conf ~/.tmux.conf
 ln -s `pwd`/.gitconfig ~/.gitconfig
 ln -s $DIR/.vimrc ~/.vimrc
-ln -s `pwd`/.bashrc ~/.bashrc.all
