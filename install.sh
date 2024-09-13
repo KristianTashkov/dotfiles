c6b1c2bf3e3c34eb35625e45518cec51f622a00a
Kristian Tashkov <kristian.tashkov@gmail.com>
2024-09-13 23:47:18 +0300

    easier ohmyzsh install

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@ DIR=`pwd`
 mkdir -p ~/.tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+# ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ln -s `pwd`/ohmyzsh/gitstatus.py ~/.oh-my-zsh/themes/gitstatus.py
+ln -s `pwd`/ohmyzsh/kris.zsh-theme ~/.oh-my-zsh/themes/kris.zsh-theme
+
 # symlinks
 ln -s `pwd`/.tmux.conf ~/.tmux.conf
 ln -s `pwd`/.gitconfig ~/.gitconfig
