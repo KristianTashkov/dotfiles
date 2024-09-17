9f49aacc25e14109554075f3a84213854aa9ebab
Kristian Tashkov <kristian.tashkov@gmail.com>
2024-09-17 23:13:56 +0300

    Add new helpful tooling from GG

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 DIR=`pwd`
+
+# brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew bundle
+
 # tmux package manager
 mkdir -p ~/.tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -9,6 +14,7 @@ ln -s `pwd`/ohmyzsh/gitstatus.py ~/.oh-my-zsh/themes/gitstatus.py
 ln -s `pwd`/ohmyzsh/kris.zsh-theme ~/.oh-my-zsh/themes/kris.zsh-theme
 
 # symlinks
-ln -s `pwd`/.tmux.conf ~/.tmux.conf
-ln -s `pwd`/.gitconfig ~/.gitconfig
+ln -s $DIR/.tmux.conf ~/.tmux.conf
+ln -s $DIR/.gitconfig ~/.gitconfig
 ln -s $DIR/.vimrc ~/.vimrc
+ln -s $DIR/.zshrc ~/.zshrc
